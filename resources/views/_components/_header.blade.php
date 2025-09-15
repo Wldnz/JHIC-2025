279bb5af358eb2074fb33f1dd7c3404a184f6277
wldnz <wildanofficial32@gmail.com>
2025-09-15 14:34:51 +0700

    something change but idint know

--- resources/views/_components/_header.blade.php
+++ resources/views/_components/_header.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-    <nav>
+    <nav class="navigation-top">
         <div class="left"><a href="#"><img src="https://smkbinainformatika.sch.id/wp-content/uploads/2022/11/logo.png"></a></div>
         <div class="center">
             <div class="links">
--- resources/views/_components/_navigation-side.blade.php
+++ resources/views/_components/_navigation-side.blade.php
@@ -1,32 +1,36 @@
+@php
+    $size = 30;
+@endphp
+
 <aside class="left open-sidebar">
     <nav class="navigation-side">
         <div class="wrapper-navigation">
             <div class="wrapper-image">
                 <img src="{{ asset("icons/default-logo.png") }}" alt="logo-bitu" height="50">
-                @include('_components._sprite-icons', ['name' => 'dashboard', "color" => "black", 'size' => 40])
+                @include('_components._sprite-icons', ['name' => 'dashboard', "color" => "black", 'size' => $size])
             </div>
             <ul class="main-menu">
                 <li id="menu-dashboard">
                     <a href="{{ route("admin.dashboard") }}">
-                        @include('_components._sprite-icons', ['name' => 'dashboard', "color" => $currentPath == 'dashboard' ? '#273B98' : 'black', 'size' => 40])
+                        @include('_components._sprite-icons', ['name' => 'dashboard', "color" => $currentPath == 'dashboard' ? '#273B98' : 'black', 'size' => $size])
                         <span>Dashboard</span>
                     </a>
                 </li>
                 <li id="menu-products">
                     <a href="{{ route("admin.products") }}">
-                        @include('_components._sprite-icons', ['name' => 'product', "color" => $currentPath == 'products' ? '#273B98' : 'black', 'size' => 40])
+                        @include('_components._sprite-icons', ['name' => 'product', "color" => $currentPath == 'products' ? '#273B98' : 'black', 'size' => $size])
                         <span>Products</span>
                     </a>
                 </li>
                 <li id="menu-transactions">
                     <a href="{{ route("admin.transactions") }}">
-                        @include('_components._sprite-icons', ['name' => 'transaction', "color" => $currentPath == 'transactions' ? '#273B98' : 'black', 'size' => 40])
+                        @include('_components._sprite-icons', ['name' => 'transaction', "color" => $currentPath == 'transactions' ? '#273B98' : 'black', 'size' => $size])
                         <span>Transactions</span>
                     </a>
                 </li>
                 <li id="menu-accounts">
                     <a href="{{ route("admin.accounts") }}">
-                        @include('_components._sprite-icons', ['name' => 'account', "color" => $currentPath == 'accounts' ? '#273B98' : 'black', 'size' => 40])
+                        @include('_components._sprite-icons', ['name' => 'account', "color" => $currentPath == 'accounts' ? '#273B98' : 'black', 'size' => $size])
                         <span>Accounts</span>
                     </a>
                 </li>
