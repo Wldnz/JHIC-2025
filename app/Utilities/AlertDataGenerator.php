f132d7a8a93bea8e0f8b083ff172dbb3e7abcf82
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2025-09-21 19:51:16 +0700

    feat: add usable alert data generator

---
+++ app/Utilities/AlertDataGenerator.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Utilities;
+
+use App\AlertType;
+
+abstract class AlertDataGenerator {
+    /**
+     * Generates an alert data array from given type, title and message.
+     *
+     * @param  AlertType $type
+     * @param  string $title
+     * @param  string $message
+     * @return array
+     */
+    public static function generateAsArray(AlertType $type, string $title, string $message) {
+        return [
+            'type' => $type->name,
+            'title' => $title,
+            'message' => $message,
+        ];
+    }
+
+    /**
+     * Generates an alert data array from given type, title and message, and merges it with given array.
+     *
+     * @param  AlertType $type
+     * @param  string $title
+     * @param  string $message
+     * @param  array $array
+     * @return array
+     */
+    public static function generateToArray(AlertType $type, string $title, string $message, array $array = []) {
+        $array['alert'] = [
+            'type' => $type,
+            'title' => $title,
+            'message' => $message,
+        ];
+        return $array;
+    }
+}
+
+?>
