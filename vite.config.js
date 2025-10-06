83e86c117bdcd5a0119fbe971bf0204210ff20c7
pefiye <pefiyeplayz@gmail.com>
2025-10-06 10:19:19 +0700

    user dashboard progress

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
     plugins: [
         laravel({
             input: [
