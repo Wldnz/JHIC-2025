7680245d4700d984cc597b4f218a7168505dd879
wldnz <wildanofficial32@gmail.com>
2025-10-06 16:02:27 +0700

    adding new pages :bannerMaker,candidates also adding default interface in admin

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- We must ship. - Taylor Otwell -->
-</div>
+@include('_components._headerAdmin', ['title' => 'Management Artikel'])
+@php
+    $currentPath = explode('/admin/', url()->current())[1];
+@endphp
+<main class="content">
+
+</main>
+
+@include('_components._footerAdmin')
