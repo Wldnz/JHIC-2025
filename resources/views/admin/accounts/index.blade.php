8ea73a2b5a5e4dbaadd6a611534bcd6475974cd6
wldnz <wildanofficial32@gmail.com>
2025-10-10 11:32:47 +0700

    adding hierrachy in navigation-side-admin

--- resources/views/admin/accounts/index.blade.php
+++ resources/views/admin/accounts/index.blade.php
@@ -1,3 +1,36 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+@include('_components._headerAdmin', ['title' => 'Account Management'])
+@php
+    $currentPath = explode('/admin/', url()->current())[1];
+@endphp
+<main class="content">
+    @include('_components._management-table', [
+        'title' => 'Akun',
+        'management' => ['title' => 'Tambahkan Pengguna',],
+        'datas' => [],
+        'columns' => [
+            'id' => 'ID Akun',
+            'full_name' => 'Nama Lengkap',
+            'email' => 'Alamat Email',
+            'phone' => 'Nomor Telepon',
+            'role' => 'Role',
+            'created_at' => 'Dibuat Pada'
+        ],
+        'findDataWith' => [
+            'filters' => [
+                'serch_role' => [
+                    'options' => [
+                        'admin' => 'Admin',
+                        'candidates' => 'Calon Peserta Didik',
+                        'article_creator' => 'Pembuat Artikel'
+                    ]
+                ],
+            ],
+            'search-engine' => [
+                'name' => 'search',
+                'placeholder' => 'Cari Nama Pengguna'
+            ]
+        ],
+    ])
+</main>
+
+@include('_components._footerAdmin')
--- resources/views/admin/transactions/index.blade.php
+++ resources/views/admin/transactions/index.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+@include('_components._headerAdmin', ['title' => 'Portfolio Management'])
+@php
+    $currentPath = explode('/admin/', url()->current())[1];
+@endphp
+<main class="content">
+    @include('_components._management-table',[
+        'title' => 'Transaksi',
+        'management' => [ 'title' => 'Tambahkan Transaksi', ],
+        'datas' => [],
+        'columns' => [
+            'ID' => 'ID Transaksi',
+            'nisn' => 'Nomor Induk Nasional',
+            'full_name' => 'Nama Lengkap',
+            'payment_method' => 'Jenis Pembayaran',
+            'status' => 'Status',  
+            'created_at' => 'Dibuat Pada'
+        ]    
+    ])
+</main>
+
+@include('_components._footerAdmin')
