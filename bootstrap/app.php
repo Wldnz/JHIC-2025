e442f181954424c8371781cf171a2692c94e5baa
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2025-10-12 04:29:32 +0700

    add: add unlighthouse package for development in js and add frame guard middleware to ensure 'X-Frame-Options' header has set up correctly

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\AlertType;
+use App\Http\Middleware\FrameGuard;
 use App\Utilities\AlertDataGenerator;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -16,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(FrameGuard::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (Exception $exception, Request $request) {
