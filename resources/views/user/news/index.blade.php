e217833022cdf7e87551234ca754684aeca9c695
Reyy344 <raihanrizky143@gmail.com>
2025-10-16 13:21:31 +0700

    add article at news page

--- resources/views/user/news/index.blade.php
+++ resources/views/user/news/index.blade.php
@@ -18,20 +18,25 @@
             @for ($i = 0; $i < 3; $i++)
             <a href="">
                 <img src="{{ asset("images/news/mamah aku menang.png") }}" alt="">
-                <div class="news-info">
-                    <div class="tags">
-                        <div class="tags-slider">
-                            <p class="tag1">Info Sekolah</p>
-                            <p class="tag2">Info PSB</p>
-                            <p class="tag3">JHIC 2025</p>
+                <div class="news-infodetail">
+                    <div class="news-info">
+                        <div class="tags">
+                            <div class="tags-slider">
+                                <p class="tag1">Info Sekolah</p>
+                                <p class="tag2">Info PSB</p>
+                                <p class="tag3">JHIC 2025</p>
+                            </div>
+                        </div>
+                        <div class="date">
+                            <p>03/12/2008</p>
                         </div>
                     </div>
-                    <div class="date">
-                        <p>03/12/2008</p>
+                    
+                    <div class="text">
+                        <h3>SEKOLAH SWASTA MENOLAK SEKOLAH GRATIS BLA BLA BLA AKU CINTA JHIC SELAMANYA TEST TEST TEST</h3>
+                        <h4>really long description of the news, one might say it's a paragraph of some sort i don't even know like bro wtf i'm just writing this for testing but wtf</h4>
                     </div>
                 </div>
-                <h3>SEKOLAH SWASTA MENOLAK SEKOLAH GRATIS BLA BLA BLA AKU CINTA JHIC SELAMANYA TEST TEST TEST</h3>
-                <h4>really long description of the news, one might say it's a paragraph of some sort i don't even know like bro wtf i'm just writing this for testing but wtf</h4>
             </a>
             @endfor
         </div>
