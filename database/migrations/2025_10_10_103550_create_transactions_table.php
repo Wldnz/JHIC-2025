d969ed536fb5ff997f551e902004eef193c220fc
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2025-10-17 03:25:28 +0700

    refactor(db): refactor transaction table

--- database/migrations/2025_10_10_103550_create_transactions_table.php
+++ database/migrations/2025_10_10_103550_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->string('candidate_nisn', 10)->nullable();
-            $table->unsignedBigInteger('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
             $table->string('candidate_full_name', 255);
             $table->string('user_email', 255);
             $table->foreignId('payment_method_id')->nullable()->constrained('payment_methods')->nullOnDelete();
