6ab7f4dc3ed537b64d98973a7a54855ae363a154
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2025-10-17 23:30:43 +0700

    edit: edit phase seeder

--- database/seeders/RegistrationPhaseSeeder.php
+++ database/seeders/RegistrationPhaseSeeder.php
@@ -18,20 +18,20 @@ class RegistrationPhaseSeeder extends Seeder
             [
                 'name' => 'Gelombang 1',
                 'quota' => 100,
-                'started_at' => '2025-01-10',
-                'ended_at' => '2025-02-15',
+                'started_at' => '2025-08-23',
+                'ended_at' => '2025-11-06',
             ],
             [
                 'name' => 'Gelombang 2',
                 'quota' => 100,
-                'started_at' => '2025-02-20',
-                'ended_at' => '2025-03-25',
+                'started_at' => '2026-02-13',
+                'ended_at' => '2026-02-14',
             ],
             [
                 'name' => 'Gelombang 3',
                 'quota' => 100,
-                'started_at' => '2025-04-01',
-                'ended_at' => '2025-04-30',
+                'started_at' => '2026-06-23',
+                'ended_at' => '2026-06-24',
             ],
         ];
 
