2cc9b5472c82a70654ea8ce5b4a94e5dd7d37054
wldnz <wildanofficial32@gmail.com>
2025-10-18 14:21:36 +0700

    make dynamic in preview-image

--- resources/views/user/galleries.blade.php
+++ resources/views/user/galleries.blade.php
@@ -13,9 +13,7 @@
     <div class="gallery-images no-fade">
         @foreach ($galleries as $gallery )
         <span class="gallery-image">
-            <img class="gallery-image-url" src="{{$gallery->url}}" alt="{{ $gallery->name }}">
-            <p class="gallery-name">{{ $gallery->name }}</p>
-            <input type="hidden" class="gallery-type" value="{{ $gallery->gallery_type_name }}">
+            <img class="gallery-image-url" src="{{$gallery->url}}" alt="{{ $gallery->name }}" data-type='{{ $gallery->gallery_type_name }}'>
         </span>
         @endforeach
         <div class="gallery-full">
@@ -99,29 +97,43 @@ ticking = true
 
 const gallery_preview_name = document.querySelector('.preview-gallery-name');
 const gallery_preview_image = document.querySelector('.preview-gallery-url');
+const wrapper_preview = document.querySelector(".gallery-full");
 const other_images = document.querySelector('.other-images');
 const galleries = @json($galleries ?? []);
 
-document.querySelectorAll(".gallery-image").forEach(el => {
-    el.addEventListener("click", () => {
-        gallery_preview_name.textContent = el.querySelector('.gallery-name').textContent;
-        gallery_preview_image.src = el.querySelector('.gallery-image-url').src;
-        document.querySelector(".gallery-full").classList.add("active")
+document.querySelectorAll(".gallery-image").forEach(el =>  el.addEventListener("click", () => setPreviewImage(el, true)));
+
+function setPreviewImage(el, hasWrapper = false){
+        let _element = el; 
+        if(hasWrapper){
+            _element  = el.querySelector('.gallery-image-url');
+        }
+
+        gallery_preview_name.textContent = _element.alt;
+        gallery_preview_image.src = _element.src;
+        wrapper_preview.classList.add("active")
         document.body.style.overflow = "hidden"
 
-        const { name, gallery_type_name } = galleries.find(g => g.name == el.querySelector('.gallery-name').textContent);
+        const { name, gallery_type_name } = galleries.find(g => g.name == _element.alt);
         if(!name || !gallery_type_name) return;
         const gs = galleries.filter(g => {
             return g.name != name && g.gallery_type_name == gallery_type_name;
         });
 
         let string_images = '';
-        gs.forEach(g => string_images += `<img src="${g.url}" alt="${g.name}">`);
+        gs.forEach(g => string_images += `<img src="${g.url}" alt="${g.name}" id='recomendation-gallery' data-type='${g.gallery_type_name}'> `);
         other_images.innerHTML = string_images;
-    })
-})
+        setRecommendationHanlder()
+}
+
+function setRecommendationHanlder(){
+    document.querySelectorAll('#recomendation-gallery').forEach(element => element.addEventListener('click', () => setPreviewImage(element) ));
+}
+
+
+
 document.querySelector(".gallery-close").addEventListener("click", () => {
-    document.querySelector(".gallery-full").classList.remove("active")
+    wrapper_preview.classList.remove("active")
     document.body.style.overflow = "auto"
 })
 
