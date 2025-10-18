3fb48a1dcf1e0815433de868a9a5f5981663b118
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2025-10-18 15:27:56 +0700

    refactor: migrate from async to queue in midtrans payment notification

--- app/Http/Controllers/MidtransController.php
+++ app/Http/Controllers/MidtransController.php
@@ -5,44 +5,81 @@ namespace App\Http\Controllers;
 use App\Models\Transaction;
 use Exception;
 use Illuminate\Http\Request;
-use Spatie\Async\Pool;
+use Illuminate\Support\Facades\Queue;
+use Laravel\SerializableClosure\SerializableClosure;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class MidtransController extends Controller
 {
+    // public function paymentNotification(Request $request) {
+    //     $data = $request->all();
+    //     $pool = Pool::create();
+
+    //     $pool[] = async(function () use ($data) {
+    //         $transaction = Transaction::query()
+    //             ->where('snap_id', '=', $data['order_id'])
+    //             ->first();
+
+    //         if (!$transaction) {
+    //             report($transaction);
+    //             logger()->error($transaction);
+    //             throw new Exception("Transaksi dengan ID {$data['order_id']} tidak ditemukan");
+    //         }
+
+    //         $transaction->status = $data['transaction_status'];
+    //         $isSaved = $transaction->save();
+
+    //         if (!$isSaved) {
+    //             report($transaction);
+    //             logger()->error($transaction);
+    //             throw new Exception("Terjadi kesalahan saat menyimpan transaksi");
+    //         }
+
+    //     })->then(function () use ($data) {
+    //         logger("Transaksi dengan ID {$data['order_id']} berhasil disimpan");
+    //     })->catch(function (Throwable $th) {
+    //         report($th);
+    //         logger()->error($th);
+    //     });
+
+
+
+    //     return response('', Response::HTTP_NO_CONTENT);
+    // }
+
     public function paymentNotification(Request $request) {
         $data = $request->all();
-        $pool = Pool::create();
 
-        $pool[] = async(function () use ($data) {
-            $transaction = Transaction::query()
-                ->where('snap_id', '=', $data['order_id'])
-                ->first();
+        Queue::push(new SerializableClosure(function () use (
+            $data
+        ) {
+            try {
+                $transaction = Transaction::query()
+                    ->where('snap_id', '=', $data['order_id'])
+                    ->first();
 
-            if (!$transaction) {
-                report($transaction);
-                logger()->error($transaction);
-                throw new Exception("Transaksi dengan ID {$data['order_id']} tidak ditemukan");
+                if (!$transaction) {
+                    logger()->error($transaction);
+                    throw new Exception("Transaksi dengan ID {$data['order_id']} tidak ditemukan");
+                }
+
+                $transaction->status = $data['transaction_status'];
+                $isSaved = $transaction->save();
+
+                if (!$isSaved) {
+                    logger()->error($transaction);
+                    throw new Exception("Terjadi kesalahan saat menyimpan transaksi");
+                }
+
+                logger("Transaksi dengan ID {$data['order_id']} berhasil disimpan");
+
+            } catch (Throwable $th) {
+                report($th);
+                logger()->error($th);
+
             }
-
-            $transaction->status = $data['transaction_status'];
-            $isSaved = $transaction->save();
-
-            if (!$isSaved) {
-                report($transaction);
-                logger()->error($transaction);
-                throw new Exception("Terjadi kesalahan saat menyimpan transaksi");
-            }
-
-        })->then(function () use ($data) {
-            logger("Transaksi dengan ID {$data['order_id']} berhasil disimpan");
-        })->catch(function (Throwable $th) {
-            report($th);
-            logger()->error($th);
-        });
-
-
+        }));
 
         return response('', Response::HTTP_NO_CONTENT);
     }
